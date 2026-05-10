318c8e43bc6751cf99d77638131d5b244d57c3bd
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-05-10 19:04:25 +0100

    update for windows

---
+++ devScripts/runIdentityService.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Starting Veil - Identity Service
+set PYTHONPATH=.
+set QUART_APP=veil/identity_service
+rem set SVC_CONFIG_FILE_REQUIRED=1
+rem set SVC_CONFIG_FILE=configs/svc.cfg
+rem set BACKEND_DB_FILENAME=databases/svc.LATEST.db
+set LOGGING_LOG_LEVEL=DEBUG
+
+quart run -p 5050 --reload
