968d7e0624599372a6ac3a83f94b69bf9ea04eb3
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-05-12 00:53:11 +0100

    Placeholders for unit tests (Common)

---
+++ tests/common/main.py
@@ -0,0 +1,6 @@
+import unittest
+from test_configuration_configuration_manager import TestConfigurationConfigurationManager
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/common/test_configuration_configuration_manager.py
@@ -0,0 +1,6 @@
+import unittest
+from veil.common.configuration_system.configuration_manager import \
+    ConfigurationManager
+
+class TestConfigurationConfigurationManager(unittest.IsolatedAsyncioTestCase):
+    ...
