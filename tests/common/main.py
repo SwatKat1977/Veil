1c78dbd966dd160f4723c4fffc21cad26532240b
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-05-17 21:41:45 +0100

    rework\

--- tests/common/main.py
+++ tests/common/main.py
@@ -1,8 +1,4 @@
 import unittest
-from tests.common.test_configuration_configuration_manager import TestConfigurationManager
-from tests.common.test_configuration_setup import (TestConfigItemDataType,
-                                                   TestConfigurationSetupItem,
-                                                   TestConfigurationSetup)
 
 
 if __name__ == "__main__":  # pragma: no cover
